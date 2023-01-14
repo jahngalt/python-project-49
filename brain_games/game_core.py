a193e0d46758374259f14c31bda0232413da568d
Oleg Kudimov <ovkudimov@gmail.com>
2023-01-14 21:44:12 +0300

    added improvements for linter pep8

--- brain_games/game_core.py
+++ brain_games/game_core.py
@@ -1,11 +1,13 @@
 import random
 import prompt
 
+
 def welcome_user():
     """Function printing welcome user message after entering name."""
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     return name
+
 
 def game_core(some_name):
     """Ask random question while score < 3"""
@@ -14,7 +16,7 @@ def game_core(some_name):
         i = random.randint(0, 100)
         print(f"Question: {i}")
         answer = str(input("Your answer: "))
-        if i % 2 == 0 and answer.lower() == 'yes': 
+        if i % 2 == 0 and answer.lower() == 'yes':
             score += 1
             print('Correct!')
         elif i % 2 != 0 and answer.lower() == 'no':
@@ -25,5 +27,4 @@ def game_core(some_name):
             answer was \'no\'.\nLet\'s try again, {some_name}!')
             break
     if score >= 3:
-        print(f"Congratulations, {some_name}!")
-            
+        print(f"Congratulations, {some_name}!\n")
